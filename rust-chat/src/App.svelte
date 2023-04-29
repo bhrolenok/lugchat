f7d327d7a60798b92b789c8fe9b5692f4b5e6998
Kudase <13634491+kudase@users.noreply.github.com>
2023-04-29 16:07:49 -0400

    (Rust) Add chat auto scroll and enter submission

--- rust-chat/src/App.svelte
+++ rust-chat/src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { onDestroy, onMount } from "svelte";
+  import { onDestroy, onMount, tick } from "svelte";
   import { listen, type UnlistenFn } from "@tauri-apps/api/event";
   // import { invoke } from "@tauri-apps/api/tauri"
 
@@ -18,44 +18,80 @@
     content: string;
   }
 
+  let chatHistory: HTMLDivElement;
   let message: string = "";
   let posts: UserMessage[] = [];
   let users: Map<string, UserDetails> = new Map();
   const listenerDeregistrations: UnlistenFn[] = [];
 
-
   onMount(async () => {
-    listenerDeregistrations.push(await listen<string>('post', (event) => {
-      let content = JSON.parse(event.payload);
-      posts.push(content);
-      posts = posts;
-    }));
+    listenerDeregistrations.push(
+      await listen<string>("post", (event) => {
+        let content = JSON.parse(event.payload);
+        posts.push(content);
+        posts = posts;
+      })
+    );
   });
 
   onDestroy(() => {
-    listenerDeregistrations.forEach(fn => {
+    listenerDeregistrations.forEach((fn) => {
       fn();
     });
   });
 
-  function postMessage() {
-    posts.push({nick: "Me", content: message, timestamp: new Date()});
-    posts=posts;
-    message="";
+  function enterKeySubmission(event) {
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault();
+      postMessage();
+    }
+  }
+
+  async function postMessage() {
+    const doScroll = isScrolledToBottom(chatHistory);
+    posts.push({ nick: "Me", content: message, timestamp: new Date() });
+    posts = posts;
+    message = "";
+
+    if (doScroll) {
+      await tick();
+      scrollToBottom(chatHistory);
+    }
+  }
+
+  function isScrolledToBottom(element: HTMLElement) :boolean {
+    // allow 1px inaccuracy by adding 1
+    return element.scrollHeight - element.clientHeight <= element.scrollTop + 1
+  }
+
+  function scrollToBottom(element: HTMLElement) {
+    element.scrollTop = element.scrollHeight - element.clientHeight
   }
 </script>
 
-<main class="container">
+<main class="container no-overflow">
   <div class="">
-    <div class="mx-3">
-      {#each posts as post, index }
-      <Post nick={post.nick} timestamp={post.timestamp} content={post.content} isReply={index % 2 == 1}></Post>
+    <div bind:this={chatHistory} class="chat-history mx-3">
+      {#each posts as post, index}
+        <Post
+          nick={post.nick}
+          timestamp={post.timestamp}
+          content={post.content}
+          isReply={index % 2 == 1}
+          />
       {/each}
+      <div id="end-of-chat" />
     </div>
-    <div class="bottom-fixed columns">
-      <textarea class="column is-three-quarters mr-2" bind:value={message} placeholder="Message" rows="1" />
-      <button class="column" on:click={postMessage}>Submit</button>
-    </div>
+  </div>
+  <div class="bottom-fixed columns">
+    <textarea
+      class="column is-three-quarters mr-2"
+      bind:value={message}
+      placeholder="Message"
+      on:keydown={enterKeySubmission}
+      rows="2"
+    />
+    <button class="column" on:click={postMessage}>Submit</button>
   </div>
 </main>
 
@@ -67,5 +103,15 @@
     padding: 0px 10px;
     width: calc(100vw - 20px);
     z-index: 50;
+  }
+  .bottom-fixed textarea {
+    resize: none;
+  }
+  .chat-history {
+    height: calc(100vh - 90px);
+    overflow: auto;
+
+    border-bottom: solid 1px #777;
+    padding-bottom: 10px;
   }
 </style>
